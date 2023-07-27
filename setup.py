0a5e9cca7df383803682746067a629d28b77bcc1
cc46808 <cc46808@gmail.com>
2023-07-27 17:52:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='Simple Mailbox Validator',
     version='1.0.0',
-    url='https://github.com/cc46808/Simple-Mailbox-Validator',
+    url='https://github.com/cc46808/Simple-Mailbox-Validator-API',
     author='Craig Carroll',
     author_email='craig@craigcarroll.design',
     description='A script to validate email addresses using the MailboxValidator API',
